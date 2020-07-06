186e93b77e6bb25791ef06f9593f59d04b94b6bc
kunming.wang <461133899@qq.com>
2020-07-06 16:19:36 +0800

    [FEATURE #00]add okhttp public domain define

--- lib_okhttp/src/main/java/com/iandroid/allclass/okhttp/config/Config.java
+++ lib_okhttp/src/main/java/com/iandroid/allclass/okhttp/config/Config.java
@@ -3,4 +3,5 @@ package com.iandroid.allclass.okhttp.config;
 public class Config {
     public static final String KEY_DOMAIN = "Domain";
     public static final String DOMAIN_NONE = KEY_DOMAIN + ":NONE";      //不指定域名 动态@Url
+    public static final String DOMAIN_PUBLIC = KEY_DOMAIN + ":PUBLIC";  //基础公共域名
 }
